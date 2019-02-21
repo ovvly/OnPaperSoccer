9d097c4fda9c607cab484e37fbafff0b43b497fe
Jakub Sowa <jakub.sowa@polidea.com>
2019-02-21 14:59:09 +0100

    Refactored MovesValidator specs

--- OnPaperSoccer/Helpers/MovesValidatorSpec.swift
+++ OnPaperSoccer/Helpers/MovesValidatorSpec.swift
@@ -13,37 +13,52 @@ class MovesValidatorSpec: QuickSpec {
                 sut = DefaultMovesValidator(fieldWidth: 42, fieldHeight: 43)
             }
 
-            sharedExamples("possible moves") { context in
-                var result: Set<Move>!
-                var moves: Set<Move>!
+            context("when current position is on left sideline") {
+                var possibleMoves: Set<Move>!
 
                 beforeEach {
-                    let point = context()["point"] as! Point
-                    moves = context()["moves"] as? Set<Move>
-
-                    result = sut.possibleMoves(from: point)
-
+                    possibleMoves = sut.possibleMoves(from: Point(x: 0, y: 0))
                 }
 
-                it("should have result the same as expectation") {
-                    expect(result) == moves
+                it("should not be possible to move left") {
+                    expect(possibleMoves).toNot(contain([Move.left, Move.upLeft, Move.downLeft]))
                 }
-            }
-
-            context("when current position is on left sideline") {
-                itBehavesLike("possible moves") {["point": Point(x: 0, y: 20), "moves": Set([Move.right, Move.upRight, Move.downRight])]}
             }
 
             context("when current position is on right sideline") {
-                itBehavesLike("possible moves") {["point": Point(x: 41, y: 20), "moves": Set([Move.left, Move.upLeft, Move.downLeft])]}
+                var possibleMoves: Set<Move>!
+
+                beforeEach {
+                    possibleMoves = sut.possibleMoves(from: Point(x: 41, y: 0))
+                }
+
+                it("should not be possible to move right") {
+                    expect(possibleMoves).toNot(contain([Move.right, Move.upRight, Move.downRight]))
+                }
             }
 
             context("when current position is on top sideline") {
-                itBehavesLike("possible moves") {["point": Point(x: 20, y: 42), "moves": Set([Move.down, Move.downLeft, Move.downRight])]}
+                var possibleMoves: Set<Move>!
+
+                beforeEach {
+                    possibleMoves = sut.possibleMoves(from: Point(x: 0, y: 42))
+                }
+
+                it("should not be possible to move up") {
+                    expect(possibleMoves).toNot(contain([Move.up, Move.upRight, Move.upLeft]))
+                }
             }
 
             context("when current position is on bottom sideline") {
-                itBehavesLike("possible moves") {["point": Point(x: 20, y: 0), "moves": Set([Move.up, Move.upLeft, Move.upRight])]}
+                var possibleMoves: Set<Move>!
+
+                beforeEach {
+                    possibleMoves = sut.possibleMoves(from: Point(x: 0, y: 0))
+                }
+
+                it("should not be possible to move down") {
+                    expect(possibleMoves).toNot(contain([Move.down, Move.downLeft, Move.downRight]))
+                }
             }
 
             pending("corners") {
