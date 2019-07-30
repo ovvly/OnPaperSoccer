e8fccdd564b1ae5c86a688cfe6d6a2eab496f7b6
Jakub Sowa <jakub.sowa@polidea.com>
2019-07-30 15:00:48 +0200

    Bar button have now correct font when highlightened

--- OnPaperSoccer/Classes/Helpers/Styles/AppStyler.swift
+++ OnPaperSoccer/Classes/Helpers/Styles/AppStyler.swift
@@ -3,6 +3,7 @@ import UIKit
 final class AppStyler {
     func applyStyle() {
         UIBarButtonItem.appearance().setTitleTextAttributes([NSAttributedString.Key.font: UIFont.App.dreamwalker(size: 24)], for: .normal)
+        UIBarButtonItem.appearance().setTitleTextAttributes([NSAttributedString.Key.font: UIFont.App.dreamwalker(size: 24)], for: .highlighted)
         UIBarButtonItem.appearance().setBackButtonTitlePositionAdjustment(UIOffset(horizontal: 0, vertical: 2.0), for: .default)
     }
 }
