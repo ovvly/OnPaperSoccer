8281de9258f585f5dfd5d9fa3b56cc928a2f83ef
Jakub Sowa <sowa.jakub@gmail.com>
2022-07-28 10:58:14 +0200

    bumped minimium iOS version to 15, fixed build issues

--- OnPaperSoccer/Classes/Helpers/Extensions/CoreGraphics+Extensions.swift
+++ OnPaperSoccer/Classes/Helpers/Extensions/CoreGraphics+Extensions.swift
@@ -25,8 +25,4 @@ extension CGFloat {
     static func /(lhs: CGFloat, rhs: Int) -> CGFloat {
         return lhs / CGFloat(rhs)
     }
-
-    static func /(lhs: Int, rhs: CGFloat) -> CGFloat {
-        return CGFloat(lhs) / rhs
-    }
 }
